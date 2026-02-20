32e981362376c315a76da486748533a447b5a36d
alexchtst-01 <acinatra@gmail.com>
2026-02-20 16:09:11 +0700

    feat: implement the data context=rpovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import DataContextProvider from "./context-provider/data-provider";
 import DataShowScreen from "./screens/data-show-screen";
 import ContentDetailDataScreen from "./screens/content-detail-data-screen";
 import SearchScreen from "./screens/search-screen";
@@ -8,15 +7,13 @@ import MainLayout from "./components/main-layout";
 export default function App() {
   return (
     <MainLayout>
-      <DataContextProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<DataShowScreen />} index />
-            <Route path="/content" element={<ContentDetailDataScreen />} />
-            <Route path="/search" element={<SearchScreen />} />
-          </Routes>
-        </Router>
-      </DataContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<DataShowScreen />} index />
+          <Route path="/content" element={<ContentDetailDataScreen />} />
+          <Route path="/search" element={<SearchScreen />} />
+        </Routes>
+      </Router>
     </MainLayout>
   );
 }
